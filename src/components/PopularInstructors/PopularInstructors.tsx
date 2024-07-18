2397abaee54fb45b5e8beac3a13b35d8541f46f3
Mehmet Alp <mehmetsunguralp54@gmail.com>
2024-07-18 17:50:07 +0300

    Instructor card and the wrapper container updated

--- src/components/PopularInstructors/PopularInstructors.tsx
+++ src/components/PopularInstructors/PopularInstructors.tsx
@@ -1,24 +1,25 @@
 import { PopularInstructorsData } from "./PopularInstructorsData";
 import { PopularInstructorsCard } from "../PopularInstructorsCard/PopularInstructorsCard";
+import styles from "./PopularInstructors.module.scss";
 
 export const PopularInstructors = () => {
-	const instructorsData = PopularInstructorsData;
-
 	return (
-		<section>
-			<h2>Top Instructors of the Month</h2>
-			{instructorsData.map((data) => {
-				return (
-					<PopularInstructorsCard
-						key={data.key}
-						name={data.name}
-						role={data.role}
-						thumbnail={data.thumbnail}
-						review={data.review}
-						numberOfStudents={data.numberOfStudents}
-					/>
-				);
-			})}
+		<section className={styles.popularInstructorsSection}>
+			<h2 className={styles.title}>Top Instructors of the Month</h2>
+			<div className={styles.instructorCards}>
+				{PopularInstructorsData.map((data) => {
+					return (
+						<PopularInstructorsCard
+							key={data.key}
+							name={data.name}
+							role={data.role}
+							thumbnail={data.thumbnail}
+							review={data.review}
+							numberOfStudents={data.numberOfStudents}
+						/>
+					);
+				})}
+			</div>
 			<p>
 				Thousands of students waiting for a instructor. Start teaching & earning now! <span>Become Instructor →</span>
 			</p>
--- src/components/PopularInstructorsCard/PopularInstructorsCard.tsx
+++ src/components/PopularInstructorsCard/PopularInstructorsCard.tsx
@@ -1,4 +1,25 @@
 import { popularInstructors } from "@/types/popularInstructors";
-export const PopularInstructorsCard = ({ name }: popularInstructors) => {
-	return <div>{name}</div>;
+import starIcon from "../../../public/assets/star.svg";
+import styles from "./PopularInstructorsCard.module.scss";
+
+export const PopularInstructorsCard: React.FC<popularInstructors> = ({ name, role, thumbnail, review, numberOfStudents }) => {
+	return (
+		<div className={styles.instructorCard}>
+			<img className={styles.instructorThumbnail} src={thumbnail} alt="" />
+			<div className={styles.personalInfo}>
+				<p className={styles.name}>{name}</p>
+				<p className={styles.title}>{role}</p>
+			</div>
+			<div className={styles.stats}>
+				<div className={styles.ratingContainer}>
+					<img src={starIcon} alt="star icon" />
+					<span className={styles.rating}>{review}</span>
+				</div>
+				<div className={styles.studentNumberContainer}>
+					<span className={styles.studentNumber}>{numberOfStudents}</span>
+					<span className={styles.students}> students</span>
+				</div>
+			</div>
+		</div>
+	);
 };
