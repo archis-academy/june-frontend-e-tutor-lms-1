ba38e89040595b964827416d2940fe07d22112c5
yusraylvz <128381663+yusraylvz@users.noreply.github.com>
2024-07-30 12:24:12 +0300

    Refactored file structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./components/Header/Header";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Header />
-    </main>
-  );
+  return <main></main>;
 }
