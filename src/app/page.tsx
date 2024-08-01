1f725ece8f6eb8afcf1b5bd1fb16cbcc2d4ce2b9
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2024-08-01 13:29:14 +0300

    [JU-4] yapıldı ve birleştirildi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { HomePageCategories } from "@/components/HomePageCategories/HomePageCategories";
 import { PopularInstructors } from "@/components/PopularInstructors/PopularInstructors";
 import { HomePageCompanies } from "@/components/HomePageCompanies/HomePageCompanies";
+import  FeatureCourse  from "@/components/FeaturedCourses/FeaturedCourses";
 
 export default function Home() {
   return (
     <main>
       <HomePageCategories />
+      <FeatureCourse/>
       <PopularInstructors />
       <HomePageCompanies />
     </main>
