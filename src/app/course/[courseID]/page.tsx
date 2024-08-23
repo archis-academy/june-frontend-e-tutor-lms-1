d75b4f64849cee87a81ffb588fe3f8cd57044308
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-08-23 22:01:44 +0300

    [JU-22]-WPD start

--- src/app/course/[courseID]/page.tsx
+++ src/app/course/[courseID]/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import CourseDetails from "../../../Components/WatchCoursePage/WatchCoursePage";
 
 const CoursePage = ({ params }: { params: { courseID: string } }) => {
-
   return (
-    <div>
-      Single Course Page {params.courseID}
-    </div>
+    <>
+      <div>Single Course Page {params.courseID}</div>
+      <div>
+        <CourseDetails />
+      </div>
+    </>
   );
 };
 
