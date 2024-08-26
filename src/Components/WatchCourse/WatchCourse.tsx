72db3ccf16412b01cb3c3a82bfc1ee4902025d7c
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-08-26 21:09:56 +0300

    [JU-22]-WPD href editing

--- src/Components/WatchCourse/WatchCourse.tsx
+++ src/Components/WatchCourse/WatchCourse.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import DescriptionTab from "../WatchCourse/DescriptionTab/DescriptionTab";
 import LectureNotesTab from "../WatchCourse/LectureNotesTab/LectureNotesTab";
 import AttachFileTab from "../WatchCourse/AttachFileTab/AttachFileTab";
@@ -6,21 +7,53 @@ import CommentsTab from "../WatchCourse/CommentsTab/CommentsTab";
 import styles from "./WatchCourse.module.scss";
 
 const WatchCourse: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<string>("description");
+
   return (
     <div className={styles.WatchContainer}>
       <nav className={styles.WatchTabs}>
-        <button>Description</button>
-        <button>Lecture Notes</button>
-        <button>
+        <a
+          href="#description"
+          className={activeTab === "description" ? "active" : ""}
+          onClick={() => setActiveTab("description")}
+        >
+          Description
+        </a>
+        <a
+          href="#lectureNotes"
+          className={activeTab === "lectureNotes" ? "active" : ""}
+          onClick={() => setActiveTab("lectureNotes")}
+        >
+          Lecture Notes
+        </a>
+        <a
+          href="#attachFile"
+          className={activeTab === "attachFile" ? "active" : ""}
+          onClick={() => setActiveTab("attachFile")}
+        >
           Attach File <span className={styles.fileCountBadge}>01</span>
-        </button>
-        <button>Comments</button>
+        </a>
+        <a
+          href="#comments"
+          className={activeTab === "comments" ? "active" : ""}
+          onClick={() => setActiveTab("comments")}
+        >
+          Comments
+        </a>
       </nav>
       <div className={styles.WatchContent}>
-        <DescriptionTab />
-        <LectureNotesTab />
-        <AttachFileTab />
-        <CommentsTab />
+        <section id="description">
+          <DescriptionTab />
+        </section>
+        <section id="lectureNotes">
+          <LectureNotesTab />
+        </section>
+        <section id="attachFile">
+          <AttachFileTab />
+        </section>
+        <section id="comments">
+          <CommentsTab />
+        </section>
       </div>
     </div>
   );
