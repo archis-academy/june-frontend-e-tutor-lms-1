30d7e9c18159562a4714f11c9847d096e7066285
Mehmet Alp <mehmetsunguralp54@gmail.com>
2024-08-26 22:48:15 +0300

    [JU-23] - Footer top component refactored

--- src/components/Footer/FooterTop/FooterTop.tsx
+++ src/components/Footer/FooterTop/FooterTop.tsx
@@ -3,28 +3,26 @@ import styles from "./FooterTop.module.scss";
 
 export const FooterTop = () => {
   return (
-    <footer className={styles.footertopbox}>
-      <div className={styles.footertopboxone}>
-        <div>
-          <p>Start learning with 67.1k students around the world.</p>
-          <div className={styles.footertopboxonesection}>
-            <button>Join the Family</button>
-            <button>Browse all courses</button>
-          </div>
+    <footer className={styles.additionalFooter}>
+      <div className={styles.startLearning}>
+        <p>Start learning with 67.1k students around the world.</p>
+        <div className={styles.buttonsWrapper}>
+          <button>Join the Family</button>
+          <button>Browse all courses</button>
         </div>
-        <div className={styles.footertopboxtwo}>
-          <div className={styles.footertopboxtwoall}>
-            <h5>6.3k</h5>
-            <p>Online courses</p>
-          </div>
-          <div className={styles.footertopboxtwoall}>
-            <h5>26k</h5>
-            <p>Certified Instructor</p>
-          </div>
-          <div className={styles.footertopboxtwoall}>
-            <h5>99.9%</h5>
-            <p>Sucess Rate</p>
-          </div>
+      </div>
+      <div className={styles.stats}>
+        <div>
+          <h5>6.3k</h5>
+          <p>Online courses</p>
+        </div>
+        <div>
+          <h5>26k</h5>
+          <p>Certified Instructor</p>
+        </div>
+        <div>
+          <h5>99.9%</h5>
+          <p>Sucess Rate</p>
         </div>
       </div>
     </footer>
