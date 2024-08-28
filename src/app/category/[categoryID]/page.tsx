86d6621f568688b870ba5d669dffce919ec11254
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-08-28 17:17:14 +0300

    [JU-19]-SPRC place setting

--- src/app/category/[categoryID]/page.tsx
+++ src/app/category/[categoryID]/page.tsx
@@ -1,14 +1,9 @@
-import { RelatedCourses } from "@/Components/RelatedCourses/RelatedCourses";
-
 import React from "react";
 
 const CategoryPage = ({ params }: { params: { categoryID: string } }) => {
   return (
     <div>
       <p>Category Page</p>
-      <div>
-        <RelatedCourses />
-      </div>
     </div>
   );
 };
--- src/app/course/[courseID]/page.tsx
+++ src/app/course/[courseID]/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { RelatedCourses } from "@/Components/RelatedCourses/RelatedCourses";
 
 const CoursePage = ({ params }: { params: { courseID: string } }) => {
-
   return (
-    <div>
-      Single Course Page {params.courseID}
-    </div>
+    <>
+      <div>Single Course Page {params.courseID}</div>
+      <div>
+        <RelatedCourses />
+      </div>
+    </>
   );
 };
 
