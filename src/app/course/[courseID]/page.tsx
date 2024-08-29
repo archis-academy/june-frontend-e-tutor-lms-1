3cdae15defa37f4749f2962db004e1952c14ceae
ygttncy <yigit03yigit@gmail.com>
2024-08-30 00:39:29 +0300

    Icon düzeltme , tasarım düzeltmeleri yapıldı

--- src/app/course/[courseID]/page.tsx
+++ src/app/course/[courseID]/page.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import Watchcourse from "@/Components/WatchcoursePageVideoSeciton/Watchcourse";
 
 const CoursePage: React.FC<{ params: { courseID: string } }> = ({ params }) => {
+  const alreadyBought = false;
   return (
     <div>
-      <h1>Single Course Page: {params.courseID}</h1>
-      <Watchcourse />
+      {alreadyBought ? (
+        <></>
+      ) : (
+        <>
+          <h1>Single Course Page: {params.courseID}</h1>
+          <Watchcourse />
+        </>
+      )}
     </div>
   );
 };
