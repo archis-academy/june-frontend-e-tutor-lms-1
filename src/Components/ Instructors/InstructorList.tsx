2472197ab52fe8fa2e906ba8b4030359616a353c
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-08-31 17:10:02 +0300

    [JU-17]-scss editing

--- src/Components/ Instructors/InstructorList.tsx
+++ src/Components/ Instructors/InstructorList.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import InstructorCard from "./InstructorCard";
 import { Instructor } from "@/types/Instructor";
-import "@/public/common/iconNima.svg";
-import "@/public/common/iconVako.svg";
+import iconVako from "@/public/common/iconVako.svg";
+import iconNima from "@/public/common/iconNima.svg";
 
 const InstructorList: React.FC = () => {
   const instructors: Instructor[] = [
     {
-      image: "iconVako.svg",
+      image: "/common/iconVako.svg",
       name: "Vako Shvili",
       title: "Web Designer & Best-Selling Instructor",
       rating: 4.9,
       students: 236568,
       courses: 9,
-      description: "One day Vako had enough with the 9-to-5 grind...",
+      description:
+        "One day Vako had enough with the 9-to-5 grind, or more like 0-to-9 in his case, and quit his job, or more like got himself fired from his own startup. He decided to work on his dream: be his own boss, travel the world, only do the work he enjoyed...READ MORE",
     },
     {
-      image: "/iconNima.svg",
+      image: "/common/iconNima.svg",
       name: "Nima Tahami",
       title: "Entrepreneur & Designer - Founder of ShiftRide",
       rating: 4.6,
       students: 5342,
       courses: 1,
       description:
-        "I'm an entrepreneur & designer with a high passion for building products...",
+        "I'm an entrepreneur & designer with a high passion for building products of all sorts and seeing ideas come to life. As a serial entrepreneur, I've designed and built projects in fields ranging from fashion to technology. I have over 10 years of experience in both...READ MORE",
     },
   ];
 
--- src/app/course/[courseID]/page.tsx
+++ src/app/course/[courseID]/page.tsx
@@ -4,11 +4,15 @@ import InstructorList from "@/Components/ Instructors/InstructorList";
 
 const CoursePage = ({ params }: { params: { courseID: string } }) => {
   return (
-    <div>
-      <h1>Single Course Page {params.courseID}</h1>
-      <StudentFeedback />
-      <InstructorList />
-    </div>
+    <>
+      <div>
+        <h1>Single Course Page {params.courseID}</h1>
+        <InstructorList />
+      </div>
+      <div>
+        <StudentFeedback />
+      </div>
+    </>
   );
 };
 
