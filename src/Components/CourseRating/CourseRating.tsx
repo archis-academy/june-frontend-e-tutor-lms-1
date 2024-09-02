e4e86f82fb54e4838bf46b70e63128cfd3b3db75
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-09-02 15:29:10 +0300

    [JU-16]-SPR title layout

--- src/Components/CourseRating/CourseRating.tsx
+++ src/Components/CourseRating/CourseRating.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./CourseRating.module.scss";
-import star from "@/public/common/starBoş.svg";
+import star from "@/public/common/emptyStar.svg";
 import bar5 from "@/public/common/bar-5.png";
 import bar4 from "@/public/common/bar-4.png";
 import bar3 from "@/public/common/bar-3.png";
@@ -42,41 +42,43 @@ const CourseRating: React.FC = () => {
       <div className={styles.ratingTitle}>
         <h3>Course Rating</h3>
       </div>
-      <div className={styles.ratingBox}>
-        <h2>4.8</h2>
-        <StarRating filledStars={4.5} />
-        <p>Course Rating</p>
-      </div>
-      <div className={styles.ratingDetails}>
-        <div className={styles.ratingRow}>
-          <StarRating filledStars={5} />
-          <p>5 Star Rating</p>
-          <img src={bar5.src} alt="5 Star Rating Bar" />
-          <span>75%</span>
+      <div className={styles.ratingContent}>
+        <div className={styles.ratingBox}>
+          <h2>4.8</h2>
+          <StarRating filledStars={4.5} />
+          <p>Course Rating</p>
         </div>
-        <div className={styles.ratingRow}>
-          <StarRating filledStars={4} />
-          <p>4 Star Rating</p>
-          <img src={bar4.src} alt="4 Star Rating Bar" />
-          <span>21%</span>
-        </div>
-        <div className={styles.ratingRow}>
-          <StarRating filledStars={3} />
-          <p>3 Star Rating</p>
-          <img src={bar3.src} alt="3 Star Rating Bar" />
-          <span>3%</span>
-        </div>
-        <div className={styles.ratingRow}>
-          <StarRating filledStars={2} />
-          <p>2 Star Rating</p>
-          <img src={bar2.src} alt="2 Star Rating Bar" />
-          <span>1%</span>
-        </div>
-        <div className={styles.ratingRow}>
-          <StarRating filledStars={1} />
-          <p>1 Star Rating</p>
-          <img src={bar1.src} alt="1 Star Rating Bar" />
-          <span>&lt; 1%</span>
+        <div className={styles.ratingDetails}>
+          <div className={styles.ratingRow}>
+            <StarRating filledStars={5} />
+            <p>5 Star Rating</p>
+            <img src={bar5.src} alt="5 Star Rating Bar" />
+            <span>75%</span>
+          </div>
+          <div className={styles.ratingRow}>
+            <StarRating filledStars={4} />
+            <p>4 Star Rating</p>
+            <img src={bar4.src} alt="4 Star Rating Bar" />
+            <span>21%</span>
+          </div>
+          <div className={styles.ratingRow}>
+            <StarRating filledStars={3} />
+            <p>3 Star Rating</p>
+            <img src={bar3.src} alt="3 Star Rating Bar" />
+            <span>3%</span>
+          </div>
+          <div className={styles.ratingRow}>
+            <StarRating filledStars={2} />
+            <p>2 Star Rating</p>
+            <img src={bar2.src} alt="2 Star Rating Bar" />
+            <span>1%</span>
+          </div>
+          <div className={styles.ratingRow}>
+            <StarRating filledStars={1} />
+            <p>1 Star Rating</p>
+            <img src={bar1.src} alt="1 Star Rating Bar" />
+            <span>&lt; 1%</span>
+          </div>
         </div>
       </div>
     </div>
