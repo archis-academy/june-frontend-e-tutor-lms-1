00411e7a45869357be01897f29046e7b2ab59ca6
yayakup <yayakup66@gmail.com>
2024-09-12 16:49:14 +0300

    [JU-13] taslak yapildi

--- src/Components/SinglecoursePageVideoSection/SingleCoursePageVideoSection.tsx
+++ src/Components/SinglecoursePageVideoSection/SingleCoursePageVideoSection.tsx
@@ -1,73 +1,54 @@
+
 "use client";
-import styles from "./SingleCoursePageVideoSection.module.scss";
-import Image from "next/image";
-import star from "../../../public/star.svg";
-import { CoursesData } from "@/utils/CoursesData";
-import { useRouter } from "next/navigation";
+import styles from "./SingleCoursePageVideoSection.module.scss"
+import Image from "next/image"
+import star from "../../../public/star.svg"
+import { CoursesData } from "@/utils/CoursesData"
+import ReactPlayer from "react-player";
 
-function SingleCoursePageVideoSection() {
-  const router = useRouter();
-  if (!router.isReady) {
-    return <p>Yükleniyor...</p>;
-  }
-
-  const { id } = router.query;
-
-  if (!id) {
-    return <p>Kurs ID'si bulunamadı.</p>;
-  }
-
-  const course = CoursesData.find((c) => c.id === id);
-
-  if (!course) {
-    return <p>Kurs bulunamadı.</p>;
-  }
-
-  return (
-    <div className={styles.singlecoursePageVideoSection}>
-      <div className={styles.singlecoursePageVideoSectionTitle}>
+function SingleCoursePageVideoSection(){
+    return(
+<div className= {styles.singlecoursePageVideoSection}>
+    <div className={styles.singlecoursePageVideoSectionTitle}>
         <div className={styles.singlecoursePageVideoSectionTitleRouter}>
-          <p>Home &gt; </p>
-          <p>{course.category} &gt; </p>
-          <p>{course.level}</p>
+            <p>Home &gt; </p> 
+            <p>Development &gt; </p>
+            <p>Web Development &gt; </p>
+            <p>Webflow</p>
         </div>
-        <h3>{course.title}</h3>
-        <p>
-          3 in 1 Course: Learn to design websites with Figma, build with Webflow, and
-          make a living freelancing.
-        </p>
+        <h3>Complete Website Responsive Design: from Figma to Webflow to Website Design</h3>
+        <p>3 in 1 Course: Learn to design websites with Figma, build with Webflow, and make a living freelancing.</p>
         <div className={styles.singlecoursePageVideoSectionInfo}>
-          <div className={styles.singlecoursePageVideoSectionInfoTrainer}>
-            <Image src={course.thumbnail} alt={course.courseOwner} />
-            <div className={styles.singlecoursePageVideoSectionTrainerName}>
-              <span>Created by:</span>
-              <p>{course.courseOwner}</p>
+            <div className={styles.singlecoursePageVideoSectionInfoTrainer}>
+                {/* <Image src:{}/> */}
+                <div className={styles.singlecoursePageVideoSectionTrainerName}>
+                    <span>Created by:</span>
+                    <p>Kristin Watson</p>
+                </div>
             </div>
-          </div>
-          <div className={styles.singlecoursePageVideoSectionInfoRating}>
-            <div>
-              <Image src={star} />
-              <Image src={star} />
-              <Image src={star} />
-              <Image src={star} />
-              <Image src={star} />
+            <div className={styles.singlecoursePageVideoSectionInfoRating}>
+                <div>
+                <Image src={star}/>
+                <Image src={star}/>
+                <Image src={star}/>
+                <Image src={star}/>
+                <Image src={star}/>
+                </div>
+                <p> 4.8 <span>(451,444 Rating)</span></p>
             </div>
-            <p>
-              {" "}
-              {course.review} <span>{course.registeredStudents}(Rating)</span>
-            </p>
-          </div>
         </div>
-      </div>
-      <div className={styles.singlecoursePageVideoSectionVideo}>
-        <video controls>
-          {/* <source src="/ArchisAcademy - Introduction" type="video/mp4" /> */}
-          <source src="/videos/ArchisAcademy-Introduction.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
     </div>
-  );
+    <div className={styles.singlecoursePageVideoSectionVideo}>
+    <ReactPlayer
+   url="https://www.youtube.com/watch?v=cL-ST_60Naw&t=5s&ab_channel=Archi%27sAcademy"
+   controls
+   width="872px"
+   height="492px"
+/>
+
+    </div>
+</div>
+    )
 }
 
 export default SingleCoursePageVideoSection;
--- src/app/course/[courseID]/page.tsx
+++ src/app/course/[courseID]/page.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import StudentFeedback from "@/Components/SingleCourseFeedback/SingleCourseFeedback";
 import { RelatedCourses } from "@/Components/RelatedCourses/RelatedCourses";
 import WatchCourse from "@/Components/WatchCourse/WatchCourse";
-import SingleCoursePageVideoSection from "../components/SingleCoursePageVideoSection/SingleCoursePageVideoSection";
+
+import SingleCoursePageVideoSection from "../../../components/SinglecoursePageVideoSection/SingleCoursePageVideoSection"
 
 const CoursePage = ({ params }: { params: { courseID: string } }) => {
-  const alreadyBought = false;
+  const alreadyBought = true;
   return (
     <>
       <div>Single Course Page {params.courseID}</div>
